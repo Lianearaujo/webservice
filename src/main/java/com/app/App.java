4da21320f6b334687db85e1cd6951449ba732cd0
Liane Araujo <lianearaujo177@gmail.com>
2022-12-03 23:33:40 -0300

    first commit

---
+++ src/main/java/com/app/App.java
@@ -0,0 +1,73 @@
+package com.app;
+import spark.ModelAndView;
+import spark.Request;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        exception(Exception.class, (e, req, res) -> e.printStackTrace()); // print all exceptions
+        staticFiles.location("/public");
+        port(8081);
+
+        get("/operations/:operation", (req, res) -> operations(req));
+
+        get("/hello", (req, res) -> "Hello World");
+    }
+
+    private static String operations(Request req) throws UnknownHostException, IOException {
+        Map<String, Object> model = new HashMap<>();
+
+        String operation = req.params(":operation");
+       
+        String result = connection_socket("127.0.0.1", 8082, operation);
+
+        model.put("nome", result);
+
+        return new VelocityTemplateEngine().render(new ModelAndView(model, "velocity/index.vm"));
+    }
+
+    private static String connection_socket(String ip, int port, String operation) {
+        try{ 
+            Socket s = new Socket(ip, port);
+
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+        
+            dos.writeUTF(operation);
+        
+            byte[] line = new byte[100];
+            dis.read(line);
+            String str = new String(line);
+            System.out.println(str.trim());
+        
+            // dis.close();
+            // dos.close();
+            // s.close();
+        
+            return str.trim();
+        }
+        catch (Exception err) {
+            System.err.println(err);
+        }
+        return "0";
+    }
+
+}
+
+
+    
+
+   
